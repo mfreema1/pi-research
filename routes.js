4e527341f9467899aa4fa418309d7986b544342a
Mark Freeman <mfreema1@stevens.edu>
2018-11-11 21:21:12 -0500

    Continue work on structure

--- routes.js
+++ routes.js
@@ -1,16 +1,25 @@
 const mysql = require('mysql');
 const dbConfig = require('./secret').dbConfig;
-const connection = mysql.createConnection(dbConfig);
-connection.connect();
+
+const _getConn = () => {
+    const conn = mysql.createConnection(dbConfig);
+    conn.connect();
+    return conn;
+}
 
 module.exports = app => {
     app.get('/', (req, res) => {
-        connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+        const conn = _getConn();
+        conn.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
             if(error) throw error;
             console.log('The solution is: ', results[0].solution);
         })
         res.send('DB hit!');
+        conn.end();
+    });
+
+    app.post('/', (req, res) => {
+        //for now just log it
+        console.log(req.body);
     });
 }
-
-connection.end();
