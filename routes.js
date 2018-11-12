88462dc6f030fb18d47a725fd6f44d1a88e77b87
Mark Freeman <mfreema1@stevens.edu>
2018-11-11 21:01:13 -0500

    Move logic to routes

--- routes.js
+++ routes.js
@@ -1,5 +1,16 @@
+const mysql = require('mysql');
+const dbConfig = require('./secret').dbConfig;
+const connection = mysql.createConnection(dbConfig);
+connection.connect();
+
 module.exports = app => {
     app.get('/', (req, res) => {
-        res.send('Hello world!');
+        connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+            if(error) throw error;
+            console.log('The solution is: ', results[0].solution);
+        })
+        res.send('DB hit!');
     });
 }
+
+connection.end();
