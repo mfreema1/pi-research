aabc7191630f03c945e90ee78817e6f166564094
mfreema1 <mfreema1@stevens.edu>
2018-11-13 11:08:42 -0500

    Partial-fix db creation script

--- utils/reset_db.js
+++ utils/reset_db.js
@@ -3,4 +3,12 @@ const dbConfig = require('../secret').dbConfig;
 const connection = mysql.createConnection(dbConfig);
 
 connection.connect();
-connection.query('SHOW TABLES;');
+connection.query("DROP TABLE IF EXISTS entries", (err, results, fields) => {
+    if(err) throw err;
+    //TODO: investigate issue with NULL being injected into datetime column, not generating DEFAULT value
+    connection.query("CREATE TABLE entries (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, datetime DATETIME DEFAULT CURRENT_TIMESTAMP, humidity FLOAT, fahrenheit FLOAT, pressure FLOAT)", (err, res, fields) => {
+        if(err) throw err;
+        console.log("New table created")
+    });
+    connection.end();
+});
