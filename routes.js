9d122fa8346dc4e19f89e9253c8f58fc49dbf1ac
mfreema1 <mfreema1@stevens.edu>
2018-11-13 11:09:05 -0500

    Fix routes to support basic functions

--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+const Promise = require('bluebird').Promise;
+const fs = Promise.promisifyAll(require('fs'));
 const dbConfig = require('./secret').dbConfig;
 
 const _getConn = () => {
@@ -11,15 +13,53 @@ module.exports = app => {
     app.get('/', (req, res) => {
         const conn = _getConn();
         conn.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-            if(error) throw error;
+            if (error) throw error;
             console.log('The solution is: ', results[0].solution);
         })
-        res.send('DB hit!');
+        res.status(200).send({ 'message': 'OK' });
         conn.end();
     });
 
+    //we'll likely need to do a batch insert for this
     app.post('/', (req, res) => {
-        //for now just log it
-        console.log(req.body);
+        const conn = _getConn();
+        const values = [req.body.datetime, req.body.humidity, req.body.fahrenheit, req.body.pressure];
+        conn.query('INSERT INTO entries (datetime, humidity, fahrenheit, pressure) VALUES (?, ?, ?, ?)', values, (error, results, fields) => {
+            if (error) {
+                res.status(400).send({ 'message': 'Bad Request' });
+                throw error;
+            }
+            console.log('Deposited new data');
+            res.status(200).send({ 'message': 'OK' }); //following AWS style
+        });
+        conn.end();
+    });
+
+    const sendData = (res) => {
+        const conn = _getConn();
+        conn.query("SELECT * FROM entries INTO OUTFILE '/tmp/data.csv'", (err, results, fields) => {
+            if (err) {
+                res.status(500).send({ 'message': 'Internal Server Error' });
+                console.log("Error forming SQL query");
+            }
+            else {
+                res.status(200).sendFile('/tmp/data.csv');
+                console.log('Send out data records');
+            }
+        });
+        conn.end();
+    };
+
+    //we may need to escape these
+    app.get('/collect', (req, res) => {
+        if (fs.existsSync('/tmp/data.csv')) {
+            fs.unlinkAsync('/tmp/data.csv')
+                .then(() => { sendData(res); })
+                .catch((err) => {
+                    res.status(500).send({ 'message': 'Internal Server Error' });
+                    console.log("Insufficient permissions, cannot remove file");
+                })
+        }
+        else sendData(res);
     });
 }
