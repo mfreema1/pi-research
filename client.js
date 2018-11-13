b380b6b9b9d3cae4562c043a0a7a5a5de4ce28f8
mfreema1 <mfreema1@stevens.edu>
2018-11-13 11:08:00 -0500

    Update client

--- client.js
+++ client.js
@@ -1,12 +1,25 @@
 const request = require('request');
 
-request.post({
+const sendWithTimestamp = () => request.post({
     url: 'http://192.168.1.200:3000',
     json: true,
     body: {
-        'Timestamp': '12:00 PM',
-        'Humidity': '40%',
-        'Pressure': '1 atm',
-        'Temperature': '72 F'
+        'Timestamp': '2018-11-13 09:25:10',
+        'Humidity': 40.434,
+        'Pressure': 1.065,
+        'Temperature': 72.567
     }
 });
+
+const sendWithoutTimestamp = () => request.post({
+    url: 'http://192.168.1.200:3000',
+    json: true,
+    body: {
+        'Humidity': 40.434,
+        'Pressure': 1.065,
+        'Temperature': 72.567
+    }
+})
+
+sendWithTimestamp();
+sendWithoutTimestamp();
