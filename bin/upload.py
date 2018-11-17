71b892e74d2e2d47c15a99f2faa7ec509f89bc3e
Mark Freeman <mfreema1@stevens.edu>
2018-11-16 23:56:16 -0500

    Update python path

--- bin/upload.py
+++ bin/upload.py
@@ -2,7 +2,7 @@ import requests
 import csv
 
 json = []
-with open('output.csv', 'r') as f:
+with open('/home/pi/pi-research/bin/output.csv', 'r') as f:
     for row in csv.DictReader(f):
         json.append(row)
 requests.post('http://52.55.77.214:3000/entries', timeout=10, json=json)
