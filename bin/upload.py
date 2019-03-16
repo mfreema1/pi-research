e8f19f2f34a62d8eef00f91988a29b7c643611ba
Mark Freeman <mfreema1@stevens.edu>
2019-03-16 14:48:35 -0400

    Add id environment variable on pi units

--- bin/upload.py
+++ bin/upload.py
@@ -1,16 +1,17 @@
-from shutil import copyfile
 import requests
 import csv
+import os
 
 json = []
 with open('/home/pi/pi-research/bin/output.csv', 'r') as f:
     for row in csv.DictReader(f):
+        if('RP_ID' in os.environ):
+            row['rp_id'] = os.environ['RP_ID']
         json.append(row)
     try:
         requests.post('http://mallard.stevens.edu:3000/entries', timeout=5, json=json)
     except: #forgive me
+        print('Could not reach mother duck')
         with open('/home/pi/pi-research/bin/archive.csv', 'a') as o:
-            f.seek(0)
-            next(f) #skip headers
             for line in f:
                 o.write(line)
